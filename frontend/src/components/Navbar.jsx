8ad288bcb2024f10834604a9d00c237d1c8c6d67
Jossuep <jossuepatric@hotmail.com>
2025-12-29 00:32:34 -0500

    Add SearchBar component and integrate search functionality in Collection page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 function Navbar() {
   const [visible, setVisible] = useState(false);
 
+  const { setShowSearch } = useContext(ShopContext);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-
-      <Link to="/"><img src={assets.logo} className="w-36" alt="Logo" /></Link>
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="Logo" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700  ">
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -29,7 +33,7 @@ function Navbar() {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img
+        <img onClick={()=>setShowSearch(true)}
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt="Search"
@@ -65,18 +69,49 @@ function Navbar() {
       {/* Sidebar menu for smalls screens  dinamic className on setVisible*/}
       <div
         className={`absolute top-0 right-0 botton-0 overflow-hidden bg-white transition-all ${
-          visible ? "w-full" : "w-0"}`}>
+          visible ? "w-full" : "w-0"
+        }`}
+      >
         <div className="flex flex-col text-gray-600">
           <div
             onClick={() => setVisible(false)}
-            className="flex items-center gap-4 p-3 cursor-pointer">
-            <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="back" />
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img
+              className="h-4 rotate-180"
+              src={assets.dropdown_icon}
+              alt="back"
+            />
             <p>Back</p>
           </div>
-          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
-          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/collections'>COLLECTIONS</NavLink>
-          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
-          <NavLink onClick={() => setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/collections"
+          >
+            COLLECTIONS
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            className="py-2 pl-6 border"
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
         </div>
       </div>
     </div>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,23 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider =(props)=>{
-    const currency = '$';
-    const delivery_fee = 10;
+const ShopContextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false); //display search bar
 
-    const value ={
-        products,
-        currency,
-        delivery_fee
-    }
-    
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
+
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
