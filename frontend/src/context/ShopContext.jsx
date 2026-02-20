18ae67e991b635a08fc2305383e5da6aa69a9643
Jossuep <jossuepatric@hotmail.com>
2026-02-20 18:34:50 -0500

    Add axios dependency and implement product data fetching in ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,20 +1,22 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
+//import { products } from "../assets/assets"; we get product data from api
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL; //into value object so any component can access to it
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false); //display search bar
   const [cartItems, setCartItems] = useState({}); //empty object as initial value
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate();
 
   const addToCart = async (itemId, size) => {
-
     if (!size) {
       toast.error("Please select a size");
       return;
@@ -37,47 +39,61 @@ const ShopContextProvider = (props) => {
 
   //function that change numbers on cart dinamically
   const getCartCount = () => {
-   let totalCount = 0;
-   //retrieve items
-     for(const items in cartItems){
+    let totalCount = 0;
+    //retrieve items
+    for (const items in cartItems) {
       //retrieve product size
-        for(const item in cartItems[items]){
-          try {
-              if (cartItems[items][item]>0) {
-                totalCount += cartItems[items][item];
-              }
-          } catch (error) {
-            
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
           }
-        }
-     }
-      return totalCount;
-  }
-
-    //update quantity function for Cart page
-    const updateQuantity = async(itemId,size,quantity)=>{
-      //copy of cartitems
-      let cartData = structuredClone(cartItems);
-      cartData[itemId][size] = quantity;//updating quantity
-      setCartItems(cartData);
+        } catch (error) {}
+      }
     }
+    return totalCount;
+  };
 
-    const getCartAmount = () =>{
-        let totalAmount = 0;
-        for(const items in cartItems){
-          let itemInfo = products.find((product)=> product._id === items);//find the product
-          for(const item in cartItems[items]){
-            try {
-              if (cartItems[items][item] > 0) {
-                  totalAmount += itemInfo.price * cartItems[items][item]; //price * quantity
-              }
-            } catch (error) {
-              
-            }
+  //update quantity function for Cart page
+  const updateQuantity = async (itemId, size, quantity) => {
+    //copy of cartitems
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity; //updating quantity
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items); //find the product
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item]; //price * quantity
           }
-        }
-        return totalAmount;
+        } catch (error) {}
+      }
     }
+    return totalAmount;
+  };
+
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list")
+      if(response.data.success){
+        setProducts(response.data.products)
+      }else{
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+  }, []);
 
   const value = {
     products,
@@ -89,9 +105,11 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
-    getCartCount,updateQuantity,
+    getCartCount,
+    updateQuantity,
     getCartAmount,
-    navigate
+    navigate,
+    backendUrl,
   };
 
   return (
@@ -100,3 +118,5 @@ const ShopContextProvider = (props) => {
 };
 
 export default ShopContextProvider;
+
+//9:30 min
